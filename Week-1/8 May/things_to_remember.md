632d201533d2f63c46cdddd5e7f36b57c2af3337
Aayush-Joshi-01 <aayushjoshi.dev@gmail.com>
2024-05-19 17:29:18 +0530

    Restructuring

--- Week-1/8 May/things_to_remember.md
+++
@@ -1,5 +0,0 @@
-# Things to Remember
-- ## Listen Carefully
-- ## Put your Suggestions
-- ## Think About the Logic
-- ## Start Working
--- Week-1/9 May/Prods/Class Codes/comprehensions.py
+++
@@ -1,6 +0,0 @@
-items = []
-for val in range(1,11):
-    items.append(val*val)
-print(items)
-
-print([item * item for item in range(1,11) if item % 2 == 0])
