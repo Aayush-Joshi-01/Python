92122c7d396dde63a5304678cc66214b1cd59686
Aayush Joshi <76811479+Aayush-Joshi-01@users.noreply.github.com>
2024-05-22 00:22:56 +0530

    commit

--- Projects/Y_Games/main_game.py
+++ Projects/Y_Games/main_game.py
@@ -4,17 +4,18 @@ from KBC.kaun_banega_crorepati import KBCGame
 from Stone_Paper_Scissor.sps import Game
 from TIC_TAC_TOE import tic_tac_toe
 
-
 import os
 import random
 import colorama
 from colorama import Fore, Back, Style
+
 
 def clear_screen():
     if os.name == 'nt':  # for windows
         _ = os.system('cls')
     else:  # for mac and linux(here, os.name is 'posix')
         _ = os.system('clear')
+
 
 def games_menu():
     os.system('color 0A')  # change bg color to light blue
@@ -27,12 +28,14 @@ def games_menu():
     print(Fore.GREEN + Style.BRIGHT + "\t5> Stone Paper Scissor")
     print(Fore.CYAN + Style.BRIGHT + "\t6> Exit")
 
+
 def print_centered(text):
     os.system('color 0A')  # change bg color to light blue
     clear_screen()
     length = len(text)
     start_pos = (os.get_terminal_size().columns - length) // 2
     print(Fore.CYAN + Style.BRIGHT + "\t" + " " * start_pos + text)
+
 
 if __name__ == '__main__':
     print_centered("Welcome to the Yash Games Services!!!!")
