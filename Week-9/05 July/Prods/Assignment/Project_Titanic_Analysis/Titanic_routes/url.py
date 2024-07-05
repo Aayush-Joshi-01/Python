5efaae32512d7175ee41eddd182476e561807647
aayushjoshi.dev <aayushjoshi.dev@gmail.com>
2024-07-05 12:34:36 +0000

    commit - numpy analysis

--- Week-9/05 July/Prods/Assignment/Project_Titanic_Analysis/Titanic_routes/url.py
+++ Week-9/05 July/Prods/Assignment/Project_Titanic_Analysis/Titanic_routes/url.py
@@ -8,21 +8,26 @@ urlpatterns: Dict[str, Callable[..., Any]] = {
     "/finance" : C_Financial.route,
     "/class" : C_Class.route,
     "/additonal" : C_Additonal.route,
+
     "/survival/overall" : C_Survival.overall_survival_rate,
     "/survival/class" : C_Survival.survival_by_class,
     "/survival/gender" : C_Survival.survival_by_gender,
     "/survival/age" : C_Survival.survival_by_age_group,
     "/survival/f_size" : C_Survival.survival_by_family_size,
+
     "/demographic/p_count_by_cls" : C_Demographic.passenger_count_by_class,
     "/demographic/gen_dist" : C_Demographic.gender_distribution,
     "/demographic/age_dist" : C_Demographic.age_distribution,
     "/demographic/embark" : C_Demographic.embarkation_port_analysis,
+
     "/finance/ticket_fare_dist" : C_Financial.ticket_fare_distribution,
     "/finance/avg_fare" : C_Financial.average_fare_by_class,
     "/finance/fare_vs_survival" : C_Financial.fare_vs_survival,
+
     "/class/pass_demo_by_cls" : C_Class.pass_demo_by_cls,
     "/class/survival_by_cls" : C_Class.survival_by_cls,
     "/class/fare_by_cls" : C_Class.fare_by_cls,
+    
     "/additonal/fam_rel_survival" : C_Additonal.family_relationships_and_survival
     }
 
